f419828b427c9dd303b9f813b42ad2a471e4c2cf
Kamil <blueflamesxcode@gmail.com>
2017-11-27 21:29:38 +0100

    Add DaoLayer class with its validator for data

--- src/main/java/rh/dao/DaoLayer.java
+++ src/main/java/rh/dao/DaoLayer.java
@@ -1,4 +1,70 @@
 package rh.dao;
 
+import rh.exceptions.DAOException;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+
 public class DaoLayer {
+    private Connection connection;
+
+    public DaoLayer() throws DAOException {
+        this.connection = DBConnection.getConnection();
+    }
+
+    public ResultSet getResultSet(String query, List<String> values) throws DAOException {
+        validate(query, values);
+
+        try {
+            PreparedStatement statement = prepareStatement(query, values);
+            return statement.executeQuery();
+
+        } catch (SQLException e) {
+            throw new DAOException("Something failed.");
+        }
+    }
+
+    public void executeCommand(String query, List<String> values) throws DAOException {
+        validate(query, values);
+
+        try {
+            PreparedStatement statement = prepareStatement(query, values);
+            statement.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new DAOException("Something failed.");
+        }
+    }
+
+    private PreparedStatement prepareStatement(String query, List<String> values) {
+        try {
+            PreparedStatement stmt = connection.prepareStatement(query);
+
+            for (int i = 0; i < values.size(); i++) {
+                int index = i + 1;
+
+                stmt.setString(index, values.get(i));
+            }
+            return stmt;
+
+        } catch (SQLException e) {
+            throw new IllegalStateException("Invalid sql query.");
+        }
+    }
+
+    private void validate(final String query, final List<String> values) throws DAOException {
+        DaoLayerValidator daoValidator = new DaoLayerValidator(query, values);
+
+        if (daoValidator.isAnyInputNull())
+            throw new DAOException("At least one value is null.");
+
+        if (!daoValidator.isEqualAmountOfInjectionsAndValues())
+            throw new DAOException("Not equal amount of injections and values delivered");
+
+    }
 }
---
+++ src/main/java/rh/dao/DaoLayerValidator.java
@@ -0,0 +1,31 @@
+package rh.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.IntPredicate;
+
+class DaoLayerValidator {
+    private final String query;
+    private final List<String> values;
+
+    DaoLayerValidator(String query, List<String> values) {
+        this.query = query;
+        this.values = values;
+    }
+
+    boolean isAnyInputNull() {
+        return this.query == null || this.values == null;
+    }
+
+    boolean isEqualAmountOfInjectionsAndValues() {
+        return countQueryInjections() == values.size();
+    }
+
+    private long countQueryInjections() {
+        IntPredicate isInjection = n -> n == '?';
+
+        return query.chars()
+                    .filter(isInjection)
+                    .count();
+    }
+}
